aad2bdad9fa6e9a6eeadc841f91ed5cb52500bbd
Michaël van Diemen <van.diemen.michael@gmail.com>
2015-02-04 09:54:51 +0100

    generate lexicons script

---
+++ lib/opener/generate_lexicons.rb
@@ -0,0 +1,39 @@
+require 'optparse'
+require 'csv'
+
+options  = {}
+lexicons = []
+file     = nil
+OptionParser.new do |opts|
+  opts.banner = "Usage: import_lexicons.rb [options]"
+
+  opts.on("-i", "--input FILE", "Input FILE") do |i|
+    options[:input] = i
+  end
+
+  opts.on("-o", "--output FILE", "Output FILE") do |o|
+    options[:output] = o
+  end
+
+  opts.on("--append", "Appends the generated lexicons to the existing output") do |a|
+    options[:append] = a
+  end
+end.parse!
+
+def write_to_file(file, lexicons, option)
+  File.open(file, option) do |file|
+    lexicons.each do |lexicon|
+      file.write "#{lexicon.join('\t')}\n"
+    end
+  end
+end
+
+CSV.foreach(options[:input], {:headers => true}) do |row|
+  lexicons << [row["lemma"], row["pos"], row["property"]]
+end
+
+if options[:append]
+  write_to_file(options[:output], lexicons, 'a')
+else
+  write_to_file(options[:output], lexicons, 'w')
+end
