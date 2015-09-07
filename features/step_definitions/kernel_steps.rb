ec7892cad2204dbe068dfecbf6e8f60794923aa5
Yorick Peterse <yorickpeterse@olery.com>
2015-09-07 11:41:43 +0200

    Don't use temp files for features
    This is not needed any more.

--- features/step_definitions/kernel_steps.rb
+++ features/step_definitions/kernel_steps.rb
@@ -1,24 +1,16 @@
 Given /^the fixture file "(.*?)"$/ do |filename|
-  @input    = fixture_file(filename)
-  @filename = filename
+  @input = fixture_file(filename)
 end
 
 Given /^I put it through the kernel$/ do
-  @tmp_filename = "output_#{rand(1000)}_#{@filename}"
-  @output       = tmp_file(@tmp_filename)
-  input         = File.read(@input)
-  output, *_    = kernel.run(input)
-
-  File.open(@output, 'w') do |handle|
-    handle.write(output)
-  end
+  input        = File.read(@input)
+  @output, *_  = kernel.run(input)
 end
 
 Then /^the output should match the fixture "(.*?)"$/ do |filename|
   fixture_output = File.read(fixture_file(filename)).strip
-  output         = File.read(@output).strip
 
-  output.should eql(fixture_output)
+  @output.should eql(fixture_output)
 end
 
 def fixture_file(filename)
