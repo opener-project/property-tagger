429b4e9aae91d33a2a329150e6571f85a4a9fea3
Braulio Oliveira <brauliooliveira@olery.com>
2020-11-09 08:52:59 -0300

    features: enforce lexicons path

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,8 @@
 require_relative '../../lib/opener/property_tagger'
 require 'rspec'
 
+ENV['PROPERTY_TAGGER_LEXICONS_PATH'] = 'tmp/lexicons/hotel'
+
 def kernel
   return Opener::PropertyTagger.new(:no_time => true, :pretty => true)
 end
